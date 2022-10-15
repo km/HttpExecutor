7f32d5fd9e26787237a29a8bea1460872d36ebd8
omar <39051822+km@users.noreply.github.com>
2022-10-16 01:18:00 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace HttpExecutor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HttpExecutor
+ .NET 6 Windows Forms tool to execute a raw HTTP request.
